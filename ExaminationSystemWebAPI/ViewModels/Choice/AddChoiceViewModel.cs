43882848bac6dc207d72047ac8232f483bb1f872
Ahmad Khaled <Ahmads20002000@gmail.com>
2024-11-18 13:56:18 +0200

    Add validation for AddChoiceViewModel

--- ExaminationSystemWebAPI/ViewModels/Choice/AddChoiceViewModel.cs
+++ ExaminationSystemWebAPI/ViewModels/Choice/AddChoiceViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using ExaminationSystemWebAPI.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExaminationSystemWebAPI.ViewModels.Choice;
 
 public class AddChoiceViewModel
 {
+    [Range(0, 4)]
     public ChoiceOrder ChoiceOrder { get; set; }
+    [Length(5,100)]
     public string TextBody { get; set; } = string.Empty;
 }
