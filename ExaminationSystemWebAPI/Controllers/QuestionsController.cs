de7a999281b028b4a51b282d17084df41d4f0227
Ahmad Khaled <Ahmads20002000@gmail.com>
2024-11-19 13:44:20 +0200

    Add QuestionsController

---
+++ ExaminationSystemWebAPI/Controllers/QuestionsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExaminationSystemWebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class QuestionsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    public IActionResult GetByID(string id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult Create()
+    {
+        return Ok();
+    }
+
+    [HttpPut]
+    public IActionResult UpdateQuestion()
+    {
+        return Ok();
+    }
+
+    [HttpPatch]
+    public IActionResult UpdateLevel()
+    {
+        return Ok();
+    }
+
+    [HttpPatch]
+    public IActionResult UpdateBody()
+    {
+        return Ok();
+    }
+
+    [HttpPatch]
+    public IActionResult UpdateScore()
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteQuestion(string id)
+    {
+        return Ok();
+    }
+}
