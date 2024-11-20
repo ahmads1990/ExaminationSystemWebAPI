ea39ebb3b3960481c5ad3dca4ff6e9761d7439fe
Ahmad Khaled <Ahmads20002000@gmail.com>
2024-11-20 19:35:32 +0200

    Add QuestionService

---
+++ ExaminationSystemWebAPI/Services/QuestionService/IQuestionService.cs
@@ -0,0 +1,12 @@
+﻿using ExaminationSystemWebAPI.Models;
+
+namespace ExaminationSystemWebAPI.Services.QuestionService;
+
+public interface IQuestionService
+{
+    IQueryable<Question> GetAll();
+    Task<Question?> GetByID(string id);
+    void Add(Question question);
+    void Delete(Question question);
+    void SaveChanges();
+}
---
+++ ExaminationSystemWebAPI/Services/QuestionService/QuestionService.cs
@@ -0,0 +1,39 @@
+﻿using ExaminationSystemWebAPI.Data.GenericRepo;
+using ExaminationSystemWebAPI.Models;
+
+namespace ExaminationSystemWebAPI.Services.QuestionService;
+
+public class QuestionService : IQuestionService
+{
+    private readonly IRepository<Question> _questionRepo;
+
+    public QuestionService(IRepository<Question> questionRepo)
+    {
+        _questionRepo = questionRepo;
+    }
+
+    public IQueryable<Question> GetAll()
+    {
+        return _questionRepo.GetAll();
+    }
+
+    public async Task<Question?> GetByID(string id)
+    {
+        return await _questionRepo.GetByID(id);
+    }
+
+    public void Add(Question question)
+    {
+        _questionRepo.Add(question);
+    }
+
+    public void Delete(Question question)
+    {
+        _questionRepo.Delete(question);
+    }
+
+    public void SaveChanges()
+    {
+        _questionRepo.SaveChanges();
+    }
+}
