45a01f3512f0a9fa8f122df73d09d54cd92e14c0
Ahmad Khaled <Ahmads20002000@gmail.com>
2024-11-20 19:28:09 +0200

    Add Question view models

---
+++ ExaminationSystemWebAPI/ViewModels/Questions/AddQuestionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ExaminationSystemWebAPI.Models;
+using ExaminationSystemWebAPI.ViewModels.Choice;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExaminationSystemWebAPI.ViewModels.Questions;
+
+public class AddQuestionViewModel
+{
+    [Range(0, 3)]
+    public QuestionLevel QuestionLevel { get; set; }
+    [Length(5, 100)]
+    public string TextBody { get; set; } = string.Empty;
+    [Range(0f, 50f)]
+    public float Score { get; set; }
+    public ICollection<AddChoiceViewModel> Choices { get; set; } = default!;
+}
---
+++ ExaminationSystemWebAPI/ViewModels/Questions/QuestionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ExaminationSystemWebAPI.ViewModels.Choice;
+
+namespace ExaminationSystemWebAPI.ViewModels.Questions;
+
+public class QuestionViewModel
+{
+    public string ID { get; set; } = string.Empty;
+    public string QuestionLevel { get; set; } = string.Empty;
+    public string TextBody { get; set; } = string.Empty;
+    public float Score { get; set; }
+    public IEnumerable<ChoiceViewModel> Choices { get; set; } = default!;
+}
