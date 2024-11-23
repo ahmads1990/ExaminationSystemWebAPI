2080d84426cc6db7d7c0c1959f01a252a1f792ff
ahmads1990 <ahmads20002000@gmail.com>
2024-11-23 19:40:32 +0200

    Add relation between Instructor and  Course

---
+++ ExaminationSystemWebAPI/Migrations/20241123173852_LinkInstructorToCourse.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExaminationSystemWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class LinkInstructorToCourse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InstructorID",
+                table: "Course",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_InstructorID",
+                table: "Course",
+                column: "InstructorID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Instructors_InstructorID",
+                table: "Course",
+                column: "InstructorID",
+                principalTable: "Instructors",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Instructors_InstructorID",
+                table: "Course");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Course_InstructorID",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "InstructorID",
+                table: "Course");
+        }
+    }
+}
--- ExaminationSystemWebAPI/Models/Course.cs
+++ ExaminationSystemWebAPI/Models/Course.cs
@@ -1,8 +1,13 @@
-﻿namespace ExaminationSystemWebAPI.Models;
+﻿using ExaminationSystemWebAPI.Models.Users;
+
+namespace ExaminationSystemWebAPI.Models;
 
 public class Course : BaseModel
 {
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public int CreditHours { get; set; }
+
+    public string InstructorID { get; set; } = string.Empty;
+    public Instructor Instructor { get; set; } = default!;
 }
