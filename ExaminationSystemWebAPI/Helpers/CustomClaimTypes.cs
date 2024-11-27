d7226fd56491df35a15801bbc423e73087f7fd82
ahmads1990 <ahmads20002000@gmail.com>
2024-11-27 17:47:10 +0200

    Refactor CustomClaimTypes fields to be constant

--- ExaminationSystemWebAPI/Helpers/CustomClaimTypes.cs
+++ ExaminationSystemWebAPI/Helpers/CustomClaimTypes.cs
@@ -2,8 +2,8 @@
 
 public static class CustomClaimTypes
 {
-    public static readonly string ISADMIN = "isAdmin";
-    public static readonly string ISINSTRUCTOR = "isInstructor";
-    public static readonly string ISSTUDENT = "isStudent";
+    public const string ISADMIN = "isAdmin";
+    public const string ISINSTRUCTOR = "isInstructor";
+    public const string ISSTUDENT = "isStudent";
     public static readonly List<string> ALLOWEDTYPES = new List<string> { ISADMIN, ISINSTRUCTOR, ISSTUDENT };
 }
