8cacc03b112a559a2e8e48d0496600a8e5ce05b5
ahmads1990 <ahmads20002000@gmail.com>
2024-11-27 17:42:22 +0200

    Add Response View Model

---
+++ ExaminationSystemWebAPI/ViewModels/ResponseViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace ExaminationSystemWebAPI.ViewModels;
+
+public class ResponseViewModel<T>
+{
+    public T Data { get; set; } = default!;
+    public bool IsSuccess { get; set; } = true;
+    public ErrorCode ErrorCode { get; set; }
+    public string Message { get; set; } = string.Empty;
+}
+
+public class SuccessResponseViewModel<T> : ResponseViewModel<T>
+{
+    public SuccessResponseViewModel(T data, string message = "")
+    {
+        Data = data;
+        IsSuccess = true;
+        Message = message;
+        ErrorCode = ErrorCode.None;
+    }
+}
+
+public class FailureResponseViewModel<T> : ResponseViewModel<T>
+{
+    public FailureResponseViewModel(ErrorCode errorCode, string message = "")
+    {
+        Data = default!;
+        IsSuccess = false;
+        Message = message;
+        ErrorCode = errorCode;
+    }
+}
+public enum ErrorCode
+{
+    None = 0,
+}
+
